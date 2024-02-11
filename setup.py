a988249b9dd8a3ece9194dba5285da8baf4ea34f
Iwo Bladek <iwo.bladek@gmail.com>
2024-02-11 22:05:24 +0100

    Minor

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 
 setup(name='evoplotter', version='0.9.0', packages=['evoplotter'])
 
-# Run the line below to install the package:
+# Run the line below to install the package in the edit mode:
 # python3 -m pip install -e <absolute path to the directory containing this path>
